2f874fa2e9729488de91e39d1b2eb3fb9581c6e2
DanielRenz <daniel.renz98@yahoo.de>
2023-12-24 17:37:59 +0100

    init

---
+++ src/HomePage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Who am I</h1>
+            <div className="avatar">
+                <div className="w-24 rounded-xl">
+                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+            </div>
+            <p>
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet,
+                ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est.
+                Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+                Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean
+                fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus
+                lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent
+                dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna
+                eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+                facilisis luctus, metus
+            </p>
+        </div>
+    );
+}
+
+export default HomePage;
